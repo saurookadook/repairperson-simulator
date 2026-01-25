b1c8b5aa48709c04434d60ca13cdb80a3ab54988
Andy Maskiell <maskiella@gmail.com>
2026-01-25 12:16:16 -0500

    feat: add a bunch more common test fixtures

--- repairperson-simulator-app/conftest.py
+++ repairperson-simulator-app/conftest.py
@@ -2,8 +2,16 @@ import logging
 import pytest
 import simpy
 from rich import inspect
+from typing import Callable
 
+from repairperson_simulator_app.simulator.config import RootConfig
+from repairperson_simulator_app.simulator.event_logger import EventLogger
 from repairperson_simulator_app.simulator.job_priority_store import JobPriorityStore
+from repairperson_simulator_app.simulator.randomizer import Randomizer
+from repairperson_simulator_app.test_factories.config import (
+    EngineConfigFactory,
+    RootConfigFactory,
+)
 from repairperson_simulator_app.utils.logging import configure_logging
 
 test_logger: logging.Logger = configure_logging("TEST_LOGGER")
@@ -18,12 +26,41 @@ def rich_inspect():
 
 
 @pytest.fixture
+def engine_config():
+    """Provides a default `EngineConfig` from `EngineConfigFactory` for tests."""
+    return EngineConfigFactory()
+
+
+@pytest.fixture
 def env():
-    """Provides a fresh SimPy environment for each test."""
+    """Provides a fresh SimPy `environment` for each test."""
     return simpy.Environment()
+
+
+@pytest.fixture
+def event_logger(env: simpy.Environment):
+    """Provides an `EventLogger` for tests."""
+    return EventLogger(env)
 
 
 @pytest.fixture()
 def job_store(env: simpy.Environment):
-    """Provides a JobPriorityStore for tests."""
+    """Provides a `JobPriorityStore` for tests."""
     return JobPriorityStore(env)
+
+
+@pytest.fixture
+def randomizer_factory() -> Callable[..., Randomizer]:
+    """
+    Provides a factory function to get a `Randomizer` for tests.
+    If no `RootConfig` is provided, uses default values from `RootConfigFactory`.
+    """
+
+    def factory(root_config: RootConfig = RootConfigFactory()):
+        return Randomizer(
+            mean_processing_time=root_config.mean_processing_time,
+            mean_time_to_failure=root_config.mean_time_to_failure,
+            sigma_processing_time=root_config.sigma_processing_time,
+        )
+
+    return factory
