6cc82409fff2cc2a0908de322067a9cf84e8e1b1
Andy Maskiell <maskiella@gmail.com>
2026-03-15 13:01:56 -0400

    feat: add thread safety to Singleton metaclass

--- repairperson-simulator-app/src/repairperson_simulator_app/utils/singleton_meta.py
+++ repairperson-simulator-app/src/repairperson_simulator_app/utils/singleton_meta.py
@@ -1,9 +1,21 @@
+import threading
+
+
 class SingletonMeta(type):
-    """A metaclass that creates a Singleton instance of the decorated class."""
+    """A metaclass that creates a Singleton instance of the decorated class.
+
+    Partially inpsired by implementation here:
+    - https://gist.github.com/werediver/4396488
+    """
+
     _instances = {}
+    __singleton_lock = threading.Lock()
+    __singleton_instance = None
 
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
-            instance = super().__call__(*args, **kwargs)
-            cls._instances[cls] = instance
+            with cls.__singleton_lock:
+                if not cls.__singleton_instance:
+                    instance = super().__call__(*args, **kwargs)
+                    cls._instances[cls] = instance
         return cls._instances[cls]
