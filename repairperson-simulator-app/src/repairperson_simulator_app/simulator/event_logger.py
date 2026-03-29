b7805d0a6090ac262a1c632c6cdda54a6d397436
Andy Maskiell <maskiella@gmail.com>
2026-03-29 19:47:59 -0400

    feat: improve EventLogger

--- repairperson-simulator-app/src/repairperson_simulator_app/simulator/event_logger.py
+++ repairperson-simulator-app/src/repairperson_simulator_app/simulator/event_logger.py
@@ -2,21 +2,18 @@ from __future__ import annotations
 
 import simpy
 
+from repairperson_simulator_app.events.base import Event
 from repairperson_simulator_app.utils.singleton_meta import SingletonMeta
 
 
 class EventLogger(metaclass=SingletonMeta):
     def __init__(self, env: simpy.Environment):
         self.env = env
-        self.events = []
+        self.events: list[Event] = []
 
-    def log_event(self, event_type: str, details: dict):
-        event = {
-            "details": details,
-            "event_type": event_type,
-            "timestamp_in_seconds": self.env.now,
-        }
+    def log_event(self, event_type: str, details: dict) -> None:
+        event = Event(type=event_type, details=details, timestamp=self.env.now)
         self.events.append(event)
 
-    def get_logged_events(self):
+    def get_logged_events(self) -> list[Event]:
         return self.events.copy()
